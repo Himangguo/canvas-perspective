8f4b06b9e8382b7d5eb270f6ebdf133d7699783f
Himangguo <809801497@qq.com>
2022-05-22 10:52:05 +0800

    feat: 增加本地视频文件上传功能

---
+++ src/canvasPerspective.js
@@ -0,0 +1,70 @@
+import { rgbaConvertToHex } from "./utils/index";
+class CanvasPerspective {
+  constructor(canvasEle, videoEle) {
+    this.videoEle = videoEle;
+    this.canvasEle = canvasEle;
+    this.context = this.canvasEle.getContext("2d");
+    this.playing = false;
+    this.gap = 2;
+    this.backgroundImageData = null; // 背景图imageData
+    // 监听video的状态
+    this.videoEle.addEventListener("play", () => {
+      console.log("play");
+      this.playing = true;
+      this.drawVideo();
+    });
+    this.videoEle.addEventListener("pause", () => {
+      console.log("pause");
+      this.playing = false;
+    });
+  }
+
+  drawVideo() {
+    this.context.drawImage(
+      this.videoEle,
+      0,
+      0,
+      this.canvasEle.width,
+      this.canvasEle.height
+    );
+    // 获取canvas像素数据
+    const imageData = this.context.getImageData(
+      0,
+      0,
+      this.canvasEle.width,
+      this.canvasEle.height
+    ).data;
+    if (!this.backgroundImageData) {
+      // 保存第一帧（背景图）的像素数据
+      this.backgroundImageData = imageData;
+    }
+    for (let i = 0; i < this.canvasEle.height; i += this.gap) {
+      for (let j = 0; j < this.canvasEle.width; j += this.gap) {
+        const pos = (i * this.canvasEle.width + j) * 4;
+        /* 
+        R - 红色（从0到255）
+        G - 绿色（从0到255）
+        B - 蓝色（从0到255）
+        A - Alpha通道（从0到255; 0是透明的，255是完全可见）
+        */
+        const red = imageData[pos];
+        const green = imageData[pos + 1];
+        const blue = imageData[pos + 2];
+        if (red > 70 && green < 60 && blue < 60) {
+          const [r, g, b, a] = this.backgroundImageData.slice(pos, pos + 4);
+          // 红色区域填充为背景色
+          this.context.fillStyle = rgbaConvertToHex(r, g, b, a);
+          this.context.fillRect(j, i, this.gap, this.gap);
+          // 图像边缘钝化
+          this.context.beginPath();
+          this.context.arc(j, i, this.gap, 0, 2 * Math.PI);
+          this.context.closePath();
+          this.context.fill();
+        }
+      }
+    }
+    this.playing && window.requestAnimationFrame(this.drawVideo.bind(this));
+  }
+}
+
+export default CanvasPerspective;
--- src/index.js
+++ src/index.js
@@ -1,75 +1,17 @@
-import redVideo from "./video/red.mp4";
-import { rgbaConvertToHex } from "./utils/index";
-class CanvasPerspective {
-  constructor(canvasEle, videoEle) {
-    this.videoEle = videoEle;
-    this.canvasEle = canvasEle;
-    this.context = this.canvasEle.getContext("2d");
-    this.videoEle.src = redVideo;
-    this.playing = false;
-    this.gap = 2;
-    this.backgroundImageData = null; // 背景图imageData
-    // 监听video的状态
-    this.videoEle.addEventListener("play", () => {
-      console.log("play");
-      this.playing = true;
-      this.drawVideo();
-    });
-    this.videoEle.addEventListener("pause", () => {
-      console.log("pause");
-      this.playing = false;
-    });
-  }
+import CanvasPerspective from "./canvasPerspective";
 
-  drawVideo() {
-    this.context.drawImage(
-      this.videoEle,
-      0,
-      0,
-      this.videoEle.width,
-      this.videoEle.height
-    );
-    // 获取canvas像素数据
-    const imageData = this.context.getImageData(
-      0,
-      0,
-      this.videoEle.width,
-      this.videoEle.height
-    ).data;
-    if (!this.backgroundImageData) {
-      // 保存第一帧（背景图）的像素数据
-      this.backgroundImageData = imageData;
-    }
-    for (let i = 0; i < this.videoEle.height; i += this.gap) {
-      for (let j = 0; j < this.videoEle.width; j += this.gap) {
-        const pos = (i * this.videoEle.width + j) * 4;
-        /* 
-        R - 红色（从0到255）
-        G - 绿色（从0到255）
-        B - 蓝色（从0到255）
-        A - Alpha通道（从0到255; 0是透明的，255是完全可见）
-        */
-        const red = imageData[pos];
-        const green = imageData[pos + 1];
-        const blue = imageData[pos + 2];
-        if (red > 70 && green < 60 && blue < 60) {
-          const [r, g, b, a] = this.backgroundImageData.slice(pos, pos + 4);
-          // 红色区域填充为背景色
-          this.context.fillStyle = rgbaConvertToHex(r, g, b, a);
-          this.context.fillRect(j, i, this.gap, this.gap);
-          // 图像边缘钝化
-          this.context.beginPath();
-          this.context.arc(j, i, this.gap, 0, 2 * Math.PI);
-          this.context.closePath();
-          this.context.fill();
-        }
-      }
-    }
-    this.playing && window.requestAnimationFrame(this.drawVideo.bind(this));
-  }
-}
+const canvasEle = document.querySelector("#canvas-perspective");
+const videoEle = document.querySelector("#video-red");
+const upload_input = document.querySelector("#upload-video");
 
-const cp = new CanvasPerspective(
-  document.querySelector("#canvas-perspective"),
-  document.querySelector("#viewo-red")
-);
+upload_input.addEventListener("change", (e) => {
+  const url = URL.createObjectURL(e.target.files[0]);
+  videoEle.src = url;
+});
+
+videoEle.addEventListener("loadeddata", () => {
+  canvasEle.width = videoEle.width;
+  canvasEle.height =
+    videoEle.videoHeight * (videoEle.width / videoEle.videoWidth);
+});
+const cp = new CanvasPerspective(canvasEle, videoEle);
